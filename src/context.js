02117bf3a3dc5297aa8163f199d46a5cec0e7260
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-01-02 14:04:34 +0500

    queryParser added to context

--- src/context.js
+++ src/context.js
@@ -1,31 +1,11 @@
-import pkg from 'graphql'
-const { parse, valueFromAST, parseValue } = pkg
-// console.log(pkg.Kind)
-function queryParser (body) {
-	const firstOperationDefinition = (ast) => ast.definitions[0]
-    const firstFieldValueNameFromOperation = (operationDefinition) => {
-        console.log(body)
-    	// console.log(operationDefinition.variableDefinitions[0].)
-        // const args = {}
-        // for(let arg of operationDefinition.selectionSet.selections[0].arguments) {
-        //     args[arg.name.value]
-        //     console.log(arg)
-        // }
-        // console.log(operationDefinition.selectionSet.selections[0].arguments)
-    	return operationDefinition.selectionSet.selections[0].name.value
-    }
-    const parsedQuery = parse(body.query)
-    // console.log(body)
-    // console.log( pkg.parseValue((new pkg.Source(body.query)).body) )
-    return {
-    	operation: firstOperationDefinition(parsedQuery).operation,
-    	fieldName: firstFieldValueNameFromOperation(firstOperationDefinition(parsedQuery)),
-    }
-}
+import queryParser from '#helpers/queryParser'
 
 export default function ({ req }) {
+	const { operation, fieldName, variables } = queryParser(req.body)
 
-	// queryParser(req.body)
+    if(fieldName === 'loginStaff') {
+        return { agent: req['headers']['user-agent'] }
+    }
 
-    return { agent: req['headers']['user-agent'] }
 }
+
---
+++ src/helpers/queryParser.js
@@ -0,0 +1,51 @@
+import { parse } from 'graphql'
+
+export default function (body) {
+    const parsedQuery = parse(body.query)
+    const fieldName = firstFieldValueNameFromOperation(firstOperationDefinition(parsedQuery))
+    const operation = firstOperationDefinition(parsedQuery).operation
+
+	function firstOperationDefinition (ast) {
+        return ast.definitions[0]
+    }
+
+    function firstFieldValueNameFromOperation (operationDefinition) {
+        return operationDefinition.selectionSet.selections[0].name.value
+    }
+
+    if(Object.keys(body.variables).length == 0) {
+        return {
+            operation,
+            fieldName,
+            variables: null,
+        }
+    }
+
+
+    const variables = variablesParser(firstOperationDefinition(parsedQuery))
+
+    function variablesParser (operationDefinition) {
+        const argsDefinition = operationDefinition.selectionSet.selections[0].arguments
+
+        for(let arg of argsDefinition) {
+            const argName = arg?.name?.value
+            const paramName = arg?.value?.name?.value
+
+            if(arg.value.kind !== 'Variable') {
+                throw new Error(`The ${argName} argument of ${fieldName} ${operation} must be Variable! Entered ${arg.value.kind}`)
+            }
+
+            if(argName !== paramName) {
+                throw new Error(`The value of ${argName} argument of ${fieldName} ${operation} must be $${argName}. Entered $${paramName}`)
+            }
+        }
+
+        return body.variables
+    }
+ 
+    return {
+    	operation,
+    	fieldName,
+        variables,
+    }
+}
