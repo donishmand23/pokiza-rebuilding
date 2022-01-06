85c667636feb59dda67951931b94d0a8ac547171
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-01-06 05:39:42 +0500

    Fixed

--- src/config.js
+++ src/config.js
@@ -3,17 +3,17 @@
 
 const PORT = process.env.PORT || 4000
 
-const PG = {
-	host: 'localhost',
-	user: 'postgres',
-	password: '2303',
-	port: 5432,
-	database: 'pokiza'
-}
+//const PG = {
+//	host: 'localhost',
+//	user: 'postgres',
+//	password: '2303',
+//	port: 5432,
+//	database: 'pokiza'
+//}
 
-// const PG = {
-// 	connectionString: 'postgres://uqziszup:bhCvhNxK8d0yAWO4mOpUcThtOTvnfh1E@kashin.db.elephantsql.com/uqziszup'
-// }
+const PG = {
+        connectionString: 'postgres://uqziszup:bhCvhNxK8d0yAWO4mOpUcThtOTvnfh1E@kashin.db.elephantsql.com/uqziszup'
+}
 
 const JWT = {
 	secretKey: '1OVH6tdmpNTjRRIqCc7rdxs01PwHzfr3',
