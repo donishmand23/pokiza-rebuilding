9bed16ac40ff70bd854ef80971252f9fd8f09d51
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-01-23 16:02:09 +0500

    Update context.js

--- src/context.js
+++ src/context.js
@@ -116,6 +116,7 @@ export default function ({ req }) {
     // only registred staffs
     if(
         [   
+            'changeOrderStatus',
             'sendNotifications',
             'sendSMS',
         ].includes(fieldName)
