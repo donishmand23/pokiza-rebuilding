430f9b7e3fca4ca9a85baedfbfd0eceab0e039d6
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-01-25 07:08:36 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@
 const PORT = process.env.PORT || 4000
 
 
-const PG = {
-	host: 'localhost',
-	user: 'postgres',
-	password: '2303',
-	port: 5432,	
-	database: 'pokiza'
-}
+// const PG = {
+// 	host: 'localhost',
+// 	user: 'postgres',
+// 	password: '2303',
+// 	port: 5432,	
+// 	database: 'pokiza'
+// }
 
 const PG = {
 	connectionString: 'postgres://vxphabyi:EC4FczOUUW-HJpFqwY3X7DRu2bTAbN7d@john.db.elephantsql.com/vxphabyi'
