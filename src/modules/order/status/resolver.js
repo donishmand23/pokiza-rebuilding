ce505876f1a3881d9d02de99f4db7ddb1c9b0e15
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-05-31 10:25:22 +0500

    Update resolver.js

--- src/modules/order/status/resolver.js
+++ src/modules/order/status/resolver.js
@@ -3,52 +3,52 @@ import statusModel from './model.js'
 const orderStatusInfo = {
 	1: {
 		code: 1,
-		name: 'moderator',
+		name: 'Moderator',
 		description: "Mijoz o'z appidan buyurtma bergan vaqt o'tadigan birinchi buyurtma statusi"
 	},
 	2: {
 		code: 2,
-		name: 'kutilmoqda',
+		name: 'Kutilmoqda',
 		description: "Xodim moderator statusidagi buyurtmani tasdiqlagan paytdagi buyurtma statusi"
 	},
 	3: {
 		code: 3,
-		name: 'biriktirilgan',
+		name: 'Biriktirilgan',
 		description: "Xodim kutilmoqda statusidagi buyurtmani olib kelish uchun mashinaga biriktirgan paytdagi buyurtma statusi"
 	},
 	4: {
 		code: 4,
-		name: 'haydovchida',
+		name: 'Haydovchida',
 		description: "Haydovchi biriktirilgan statusli buyurtma ichiga mijozdan qabul qilib olgan buyumlarni birinchi marta kiritib, mashinaga yuklagan paytdagi buyurtma statusi"
 	},
 	5: {
 		code: 5,
-		name: 'jarayonda',
+		name: 'Jarayonda',
 		description: "Buyumlari 'yetib kelgan', 'yuvilishda', 'quritishda', 'qadoqlashda', 'omborda' statuslaridan birida bo'lgan paytdagi buyurtma statusi"
 	},
 	6: {
 		code: 6,
-		name: 'tayyor',
+		name: 'Tayyor',
 		description: "Buyumlarining barchasi 'tayyor' bo'lgan paytdagi buyurtma statusi"
 	},
 	7: {
 		code: 7,
-		name: 'yuklangan',
+		name: 'Yuklanmoqda',
 		description: "Tayyor statusidagi buyurtmani yetkazib berish uchun mashinaga biriktirilgan paytdagi buyurtma statusi"
 	},
 	8: {
 		code: 8,
-		name: 'yetkazib berishda',
+		name: 'Yetkazib berishda',
 		description: "Yetkazib berish uchun mashinaga biriktirilgan buyurtmani, mashinaga har bir buyumi bilan yuklangan paytdagi buyurtma statusi"
 	},
 	9: {
 		code: 9,
-		name: 'yetkazildi',
+		name: 'Yetkazildi',
 		description: "Buyurtma har bir buyumi bilan to'liq mijozga yetkazib berilgan paytdagi buyurtma statusi"
 	},
 	10:{
 		code: 10,
-		name: 'qisman yetkazildi',
+		name: 'Qisman yetkazildi',
 		description: "Buyurtma yetkazib berilganda, ichidagi bazi buyumlari qaytarib yuborilgan paytdagi buyurtma statusi"
 	},
 }
@@ -91,7 +91,7 @@ const productStatusInfo = {
 	},
 	8: {
 		code: 8,
-		name: 'Yuklangan',
+		name: 'yuklanmoqda',
 		description: 'Yetkazib berish uchun mashinaga biriktirilgan buyum statusi'
 	},
 	9: {
